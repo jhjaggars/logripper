f77c4acaac8522ab1afe437c9b43b7eb909df7ea
Jesse Jaggars <jjaggars@redhat.com>
2019-04-16 15:25:03 -0500

    using cloudwatch logging too

--- logripper.py
+++ logripper.py
@@ -3,14 +3,28 @@ import random
 import sys
 import time
 import logging
+from boto3.session import Session
+import watchtower
 from logstash_formatter import LogstashFormatterV1
 from itertools import count
 
 if any("KUBERNETES" in k for k in os.environ):
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(LogstashFormatterV1())
+
+    boto3_session = Session(
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+        region_name="us-east-1")
+
+    wt_handler = watchtower.CloudWatchLogHandler(
+        boto3_session=boto3_session,
+        log_group="logripper",
+        stream_name="testing")
+
     logging.root.setLevel("INFO")
     logging.root.addHandler(handler)
+    logging.root.addHandler(wt_handler)
 else:
     logging.basicConfig(level=logging.INFO)
 
